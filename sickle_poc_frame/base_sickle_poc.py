76b67f9f6f5085271c74c36b0347a221b4347d74
bsmali4 <bsmali4@bsmali4deMacBook-Pro.local>
2017-04-20 17:03:47 +0800

    first commit

--- sickle_poc_frame/base_sickle_poc.py
+++ sickle_poc_frame/base_sickle_poc.py
@@ -1,30 +1,5 @@
-#coding=utf-8
-'''
-    SicklePoc基础父类
-    功能说明
-    只制作漏洞验证模块
-    基础信息
-    self.info = {'author':'bsmali4','name':'zabnix-2013-9-12','imp_version':'3.2-4.5','description':'zabbix前台注入,
-    参考链接xxx','repair':'升级zabbix', 'type': BugType.sqlInject}
-    self.option = {"url": "http://www.codersec.net","port": "80","user-agent":"Mozilla/5.0 (compatible; Googlebot/2.1"}
-    self.result = {"target":"http://www.baidu.com","exit": "True","bugtype":"injection","description":[{"type":"injection","result":"username:admin|password:admin12345"},{"type":"xss","result":"xss?alert(1)"}]}
-    sqlinject = '注入' hight
-    xss = 'xss跨站脚本攻击' hight
-    xxe = 'xml外部实体攻击' hight
-    csrf = '跨站请求伪造' middle
-    cors = '请求对象共享' hight
-    jsonp = 'jsonp劫持' middle
-    weakpwd = '弱密码' hight
-    crlf = '回车键注入' low
-    infoleak = '信息泄漏' hight
-    cmdect = '命令执行' hight
-    ddos = 'ddos漏洞' hight
-    fileread = '任意文件读取,包含任意下载之类的' hight
-    fileupload = '任意文件上传' hight
-    fileinclude = '文件包含' hight
-    other = '其它,包括越权之类的' middle
+# coding=utf-8
 
-'''
 import sys
 sys.path.append("../..")
 import argparse
